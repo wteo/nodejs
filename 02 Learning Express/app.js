132598bf0a3b961772f2fa90852b584cfa6110a4
Wendy Teo <wendyteo.wy@gmail.com>
2023-08-03 12:24:47 +1000

    parsed form

--- 02 Learning Express/app.js
+++ 02 Learning Express/app.js
@@ -1,18 +1,19 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+import { form } from './templates.js';
 
 const app = express(); // creating a new app via express which a lot of logic is all stored behind the scnee
 
-app.use('/', (req, res, next) => {
-  console.log('This always runs');
-  next();
+app.use(bodyParser.urlencoded());
+
+app.use('/form', (req, res) => {
+  res.send(form);
 });
 
-app.use('/addon', (req, res, next) => {
-  res.send('<p>This is the add-on page</p>');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<p>This is the main page</p>');
+app.use('/submit', (req, res, next) => {
+  console.log(req.body); // this needs to be parsed first
+  res.redirect('/form');
 });
 
 const port = 8000;
