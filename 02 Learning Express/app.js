b622c2795f8220f37ddaf881e4f87e6e99a8007a
Wendy Teo <wendyteo.wy@gmail.com>
2023-08-04 11:16:15 +1000

    Added templates

--- 02 Learning Express/app.js
+++ 02 Learning Express/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
 import formRouter from './routes/form.js';
 
-const app = express(); // creating a new app via express which a lot of logic is all stored behind the scnee
+const app = express(); // creating a new app via express which a lot of logic is all stored behind the scene
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -12,7 +17,7 @@ app.use('/user', formRouter);
 // A catch all route
 app.use((req, res) => {
   res.status(404);
-  res.send('<p>Page Not Found.</p>')
+  res.sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 const port = 8000;
--- 02 Learning Express/routes/form.js
+++ 02 Learning Express/routes/form.js
@@ -1,30 +1,14 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
 const router = express.Router();
 
-const form = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>User Information Form</title>
-    </head>
-    <body>
-        <h1>User Information Form</h1>
-        <form action="/user/submit" method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required>
-        <br>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
-        <br>
-        <button type="submit">Submit</button>
-        </form>
-    </body>
-    </html>
-    `;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 router.get('/form', (req, res) => {
-  res.send(form);
+  res.sendFile(path.join(__dirname, '../', 'views', 'form.html'));
 });
 
 router.post('/submit', (req, res) => {
