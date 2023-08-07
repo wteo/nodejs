406f05b822c92b9cdddaa6bea13df97173b6e721
Wendy Teo <wendyteo.wy@gmail.com>
2023-08-07 10:50:46 +1000

    Remove comment

--- 02 Learning Express/Assignment4/app.js
+++ 02 Learning Express/Assignment4/app.js
@@ -28,9 +28,3 @@ app.listen(port, () => {
     console.log(`Listening to Port: ${port}`)
 });
 
-
-// 1. create a npm project and install Express.js
-// 2. Add two routes
-//   - "/" => Holds a <form> that allows user to input their name
-//   - "/users" => Outputs an <ul> with the user names (or some error text) 
-// Make sure you also include layoutes via "includes"
