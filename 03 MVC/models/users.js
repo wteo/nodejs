9cbf0a3fd7df07954b448c2d277974e12604bfdf
Wendy Teo <wendyteo.wy@gmail.com>
2023-08-09 16:27:13 +1000

    Set up package

--- 03 MVC/models/users.js
+++ 03 MVC/models/users.js
@@ -4,17 +4,18 @@ import path from 'path';
 import { fileURLToPath } from 'url'; 
 
 const __filename = fileURLToPath(import.meta.url); 
-const __dirname = path.dirname(__filename);  
+const __dirname = path.dirname(__filename); 
+
+const dataFilePath = path.join(__dirname, '..', 'data', 'usernames.json');
 
 class UserModel {
     constructor(callback) {
-        this.dataFilePath = path.join(__dirname, '..', 'data', 'usernames.json');
         this.usernames = [];
         this.loadUsersFromFile(callback);
     }
 
     loadUsersFromFile(callback) {
-        fs.readFile(this.dataFilePath, 'utf-8', (err, data) => {
+        fs.readFile(dataFilePath, 'utf-8', (err, data) => {
             if (err) {
                 console.error('Error reading from file:', err);
                 callback([]); // Return an empty array in case of an error
@@ -26,7 +27,7 @@ class UserModel {
     }
 
     saveUsersToFile() {
-        fs.writeFile(this.dataFilePath, JSON.stringify(this.usernames, null, 2), (err) => {
+        fs.writeFile(dataFilePath, JSON.stringify(this.usernames, null, 2), (err) => {
             if (err) {
                 console.error('Error writing to file:', err);
             }
